7e12d8c0e5a0db88cfeb244ea37787f17bf7da7a
marmarti <markwinap@gmail.com>
2022-05-03 00:22:45 -0500

    Update bot version

--- azdhs.py
+++ azdhs.py
@@ -712,7 +712,7 @@ class NewprojectApp:
 
         # Version Footer
         self.label2 = tk.Label(self.frame2)
-        self.label2.configure(background='#ffffff', text="Version 2.0")
+        self.label2.configure(background='#ffffff', text="Version 3.0")
         self.label2.pack(side='top')
         self.frame2.configure(background='#ffffff', height='200', width='200')
         self.frame2.pack(side='top')
